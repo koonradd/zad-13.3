b94ec184c3ed483eea224eef4a0fdb4aec99f5bb
koonradd <35108423+koonradd@users.noreply.github.com>
2018-02-11 22:34:50 +0100

    Update program.js

--- program.js
+++ program.js
@@ -14,15 +14,14 @@
 });*/
 
 console.log('node version: ' + process.versions.node);
-console.log(process.env.LANG);
+
 
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
-var input = process.stdin.read();
-    if (input !== null) {
-         var instruction = input.toString().trim();
-        
- switch(instruction) {
+    var input = process.stdin.read();
+    if(input !== null) {
+        var instruction = input.trim();
+        switch(instruction) {
             case '/exit':
                 process.stdout.write('Quitting app!\n'');
                 process.exit();
